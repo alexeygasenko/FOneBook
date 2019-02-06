5c96a76cf97aee20c34fc997fc34affd356979ac
Alexey Gasenko <gboydf@yandex.ru>
2019-02-06 13:22:36 +0500

    Rename App component to Main

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
-import './App.css';
+import './Main.css';
 
-export default class App extends Component {
+export default class Main extends Component {
   render() {
     return (
       <div className="App">
