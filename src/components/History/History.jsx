317e9ad42bd0fb4611f92614804da5d989e4a350
Alexey Gasenko <gboydf@yandex.ru>
2019-02-08 21:40:11 +0500

    Add images to History component

--- src/components/History/History.jsx
+++ src/components/History/History.jsx
@@ -3,17 +3,23 @@ import { Card, CardImg, Button } from 'reactstrap';
 import './History.css';
 import Footer from '../Footer/Footer';
 
+import drivers from '../../data/img/history/drivers.jpg';
+import grandPrix from '../../data/img/history/gp.jpg';
+import tracks from '../../data/img/history/tracks.jpg';
+import auto from '../../data/img/history/auto.jpg';
+
 export default class History extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = {
       historySections: ['Гонщики', 'Гран-при', 'Трассы', 'Автомобили'],
+      historyImages: [drivers, grandPrix, tracks, auto],
     };
   }
 
   renderSections = () => {
-    const { historySections } = this.state;
+    const { historySections, historyImages } = this.state;
     let sections = [];
 
     for (let i = 0; i < historySections.length; ++i) {
@@ -23,7 +29,7 @@ export default class History extends React.Component {
             <CardImg
               className="history-img"
               top
-              src="https://placeholdit.imgix.net/~text?txtsize=33&txt=320%C3%97320&w=320&h=320"
+              src={historyImages[i]}
               alt="Card image cap"
             />
             <Button className="read-more">{historySections[i]}</Button>
