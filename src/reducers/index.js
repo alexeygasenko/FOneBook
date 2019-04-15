b2169d30e77e5808ad3694669ed51deeef9c9c28
Alexey Gasenko <gboydf@yandex.ru>
2019-04-15 22:38:43 +0500

    Add NewsFeed connection with the server

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import errorReducer from './errorReducer';
 import authReducer from './authReducer';
+import { newsFeedReducer } from './newsFeedReducer';
 
 export default combineReducers({
   errors: errorReducer,
   auth: authReducer,
+  newsFeedReducer: newsFeedReducer,
 });
